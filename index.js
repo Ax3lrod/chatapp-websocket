855012c9543ec7ee7a79067b4c09ed27af287094
Ax3lrod <aryasatyagigachad9@gmail.com>
2025-05-08 01:41:48 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("new user", (username) => {
+    console.log(username + " has joined the chat");
+    socket.username = username;
+    socket.broadcast.emit("new user", username);
+  });
+
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+    io.emit("chat message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
